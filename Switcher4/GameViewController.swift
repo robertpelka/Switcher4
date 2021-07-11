1f3bbe3f13d42665c82dd6623ffd6e08fc9b7873
Robert Pelka <123455ff@gmail.com>
2021-07-11 22:03:45 +0200

    Add animations

--- Switcher4/GameViewController.swift
+++ Switcher4/GameViewController.swift
@@ -16,6 +16,9 @@ class GameViewController: UIViewController {
     
     let cameraNode = SCNNode()
     let lightNode = SCNNode()
+    let playerNode = SCNNode()
+    
+    var animations = [String: CAAnimation]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,17 +30,21 @@ class GameViewController: UIViewController {
         setupCamera()
         setupLight()
         setupFloor()
+        setupPlayer()
         loadAnimations()
+        setupGestures()
     }
     
     func setupScene() {
         sceneView = (view as! SCNView)
+        sceneView.delegate = self
+        
         scene = SCNScene()
         sceneView.scene = scene
         
         sceneView.backgroundColor = UIColor(red: 255/255, green: 210/255, blue: 138/255, alpha: 1)
         sceneView.showsStatistics = true
-        sceneView.allowsCameraControl = true
+        //sceneView.allowsCameraControl = true
     }
     
     func setupCamera() {
@@ -85,15 +92,98 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(floorNode)
     }
     
-    func loadAnimations() {
-        let runScene = SCNScene(named: "art.scnassets/maria/run.dae")!
-        let node = SCNNode()
-        for child in runScene.rootNode.childNodes {
-            node.addChildNode(child)
+    func setupPlayer() {
+        let runningScene = SCNScene(named: "art.scnassets/maria/run.dae")!
+        for child in runningScene.rootNode.childNodes {
+            playerNode.addChildNode(child)
         }
-        node.scale = SCNVector3(x: 0.01, y: 0.01, z: 0.01)
-        node.eulerAngles = SCNVector3(x: 0, y: convertToRadians(degrees: 180), z: 0)
-        sceneView.scene?.rootNode.addChildNode(node)
+        playerNode.scale = SCNVector3(x: 0.01, y: 0.01, z: 0.01)
+        playerNode.eulerAngles = SCNVector3(x: 0, y: convertToRadians(degrees: 180), z: 0)
+        let runningAction = SCNAction.repeatForever(SCNAction.moveBy(x: 0, y: 0, z: -3.5, duration: 1.0))
+        playerNode.runAction(runningAction)
+        sceneView.scene?.rootNode.addChildNode(playerNode)
+    }
+    
+    func loadAnimations() {
+        loadAnimation(from: "attack", fadeInDuration: 0.1, fadeOutDuration: 1.15)
+        loadAnimation(from: "slide", fadeInDuration: 0.1, fadeOutDuration: 0.2)
+        loadAnimation(from: "jump", fadeInDuration: 0.02, fadeOutDuration: 0.02)
+        loadAnimation(from: "left", fadeInDuration: 0.1, fadeOutDuration: 0.1)
+        loadAnimation(from: "right", fadeInDuration: 0.1, fadeOutDuration: 0.1)
+        loadAnimation(from: "roll", fadeInDuration: 0.1, fadeOutDuration: 0.1)
+    }
+    
+    func loadAnimation(from name: String, fadeInDuration: CGFloat, fadeOutDuration: CGFloat) {
+        let sceneURL = Bundle.main.url(forResource: "art.scnassets/maria/" + name, withExtension: "dae")
+        let sceneSource = SCNSceneSource(url: sceneURL!, options: nil)
+        if let animationObject = sceneSource?.entryWithIdentifier(name + "-1", withClass: CAAnimation.self) {
+            animationObject.repeatCount = 1
+            animationObject.fadeInDuration = fadeInDuration
+            animationObject.fadeOutDuration = fadeOutDuration
+            animationObject.isRemovedOnCompletion = true
+            animations[name] = animationObject
+        }
+    }
+    
+    func setupGestures() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture))
+        sceneView.addGestureRecognizer(tapGesture)
+        
+        let swipeUpGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeGesture))
+        swipeUpGesture.direction = .up
+        sceneView.addGestureRecognizer(swipeUpGesture)
+        
+        let swipeDownGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeGesture))
+        swipeDownGesture.direction = .down
+        sceneView.addGestureRecognizer(swipeDownGesture)
+        
+        let swipeRightGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeGesture))
+        swipeRightGesture.direction = .right
+        sceneView.addGestureRecognizer(swipeRightGesture)
+        
+        let swipeLeftGesture = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeGesture))
+        swipeLeftGesture.direction = .left
+        sceneView.addGestureRecognizer(swipeLeftGesture)
+    }
+    
+    @objc func handleSwipeGesture(_ sender: UISwipeGestureRecognizer) {
+        switch sender.direction {
+            case .up:
+                runAnimation(named: "jump")
+            case .down:
+                runAnimation(named: "slide")
+            case .right:
+                runAnimation(named: "right")
+            case .left:
+                runAnimation(named: "left")
+            default:
+                break
+        }
+    }
+    
+    @objc func handleTapGesture(_ sender: UISwipeGestureRecognizer) {
+        runAnimation(named: "attack")
+    }
+    
+    func runAnimation(named name: String) {
+        if !playerNode.animationKeys.contains(name) {
+            playerNode.addAnimation(animations[name]!, forKey: name)
+        }
     }
 
+}
+
+//MARK: - SCNSceneRendererDelegate
+
+extension GameViewController: SCNSceneRendererDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, didApplyAnimationsAtTime time: TimeInterval) {
+        updateCameraPosition()
+    }
+    
+    func updateCameraPosition() {
+        let differenceZ = playerNode.position.z + 3.2 - cameraNode.position.z
+        cameraNode.position.z += differenceZ
+    }
+    
 }
